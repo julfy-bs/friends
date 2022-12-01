acafccba820440ec34787e4b136df2a56f7b5034
bator <batr.fly@mail.ru>
2022-12-01 22:59:11 +0400

    add carousel

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
 function initSwiper(element) {
   const swiperElement = element.querySelector('.swiper');
+  const nextEl = element.querySelector('.carousel__arrow_type_next');
+  const prevEl = element.querySelector('.carousel__arrow_type_prev');
   // eslint-disable-next-line no-undef
-  const swiper = new Swiper(swiperElement);
+  const swiper = new Swiper(swiperElement, {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+    navigation: {
+      nextEl,
+      prevEl,
+    },
+    loop: true,
+    spaceBetween: 30,
+    autoplay: {
+      delay: 15000,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
+    },
+  });
   return swiper;
 }
 
-const carouselElements = document.querySelectorAll('[data-carousel]');
+const carouselElements = document.querySelectorAll('.carousel');
 carouselElements.forEach(initSwiper);
