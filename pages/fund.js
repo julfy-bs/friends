ba6c952317324223aba518d0d5c3f97f276b0e71
Bogdan Sutuzhko <julfy-bs@yandex.ru>
2022-12-10 16:54:42 +0300

    feat: вносит изменения в хедер, футер, js файлы, добавляет попап и дропдаун.
    добавляет popup и dropdown для хедера, переиспользует компоненты для футера, хедера и модальных окон. Добавляет стили. Изменяет структуру хедера. Добавляет хедер, футер и компоненты модальных окон на страницу "Помочь Фонду".

--- pages/fund.js
+++ pages/fund.js
@@ -1,5 +1,6 @@
-const popupForm = document.querySelector('.popup-form');
-const popupProfileCloseButton = popupForm.querySelector('.popup__close');
+// popup форма успешно отправлена
+const popupForm = document.querySelector('.popup_role_form');
+const popupFormCloseButton = popupForm.querySelector('.popup__close');
 const form = document.querySelector('.connection__form');
 const formName = form.querySelector("[name='name']");
 const formEmail = form.querySelector("[name='email']");
@@ -24,5 +25,37 @@ form.addEventListener('submit', (e) => {
 });
 
 popupForm.addEventListener('click', (e) => {
-  if (e.target === popupForm || e.target === popupProfileCloseButton) closePopup(popupForm);
+  if (e.target === popupForm || e.target === popupFormCloseButton) closePopup(popupForm);
+});
+
+// burger-menu
+const hamburgerMenu = document.querySelector('.hamburger_role_menu');
+const popupBurger = document.querySelector('.popup_role_burger');
+const popupBurgerCloseButton = popupBurger.querySelector('.popup__close');
+
+hamburgerMenu.addEventListener('click', () => {
+  openPopup(popupBurger);
+});
+
+popupBurgerCloseButton.addEventListener('click', () => {
+  closePopup(popupBurger);
+});
+
+// drop-menu
+const hamburgerDrop = document.querySelector('.hamburger_role_drop-menu');
+const dropMenu = document.querySelector('.drop-menu');
+
+const toggleDropMenu = () => {
+  const condition = dropMenu.classList.contains('drop-menu_active') && hamburgerDrop.classList.contains('hamburger_active');
+  if (condition) {
+    dropMenu.classList.remove('drop-menu_active');
+    hamburgerDrop.classList.remove('hamburger_active');
+  } else {
+    dropMenu.classList.add('drop-menu_active');
+    hamburgerDrop.classList.add('hamburger_active');
+  }
+};
+
+hamburgerDrop.addEventListener('click', () => {
+  toggleDropMenu();
 });
